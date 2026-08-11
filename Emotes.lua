f0c58cab08fc47135ea1982ee6ec97d707d25a7a
sawakotzy <kimchiudonn@gmail.com>
2026-08-11 14:56:58 +0800

    Woah

--- Emotes.lua
+++ Emotes.lua
@@ -1,1 +1,4 @@
-{Name = "Don’t Start Now by Motion CO.", Id = 124871427411900, Price = 50, Index = #totalEmotes+9},
+return {
+	{Name = "Don’t Start Now by Motion CO.", Id = 124871427411900, Price = 50},
+	{Name = "Another Emote", Id = 123456789012345, Price = 25},
+}
